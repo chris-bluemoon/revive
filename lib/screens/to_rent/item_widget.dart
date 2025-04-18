db83fbaaa2684dd5f28e8224db52fcd889d2ea86
Ye Nay Thway <yenaythway77@gmail.com>
2025-04-18 18:05:23 +0700

    fix slow initializing images in item widget

--- lib/screens/to_rent/item_widget.dart
+++ lib/screens/to_rent/item_widget.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:revivals/models/item.dart';
@@ -13,63 +12,61 @@ class ItemWidget extends StatefulWidget {
   final int itemNumber;
   final Item item;
 
-
-
   @override
   State<ItemWidget> createState() => _ItemWidgetState();
 }
 
 class _ItemWidgetState extends State<ItemWidget> {
-  
   late String itemType;
   late String itemName;
   late String brandName;
   late String imageName;
   late Image thisImage;
 
-  Image setItemImage() {
-    // itemType = toBeginningOfSentenceCase(widget.item.type.replaceAll(RegExp(' +'), '_'));
-    // itemName = widget.item.name.replaceAll(RegExp(' +'), '_');
-    // brandName = widget.item.brand.replaceAll(RegExp(' +'), '_');
-    // imageName = 'assets/img/items2/${brandName}_${itemName}_${itemType}_${widget.itemNumber}.jpg';
-    // return imageName;
-    for (ItemImage i in Provider.of<ItemStore>(context, listen: false).images) {
-      for (String j in widget.item.imageId) {
-        if (i.id == j) {
-          images.add(i.imageId);
-        }
-      }
-    }
-    for (ItemImage i in Provider.of<ItemStore>(context, listen: false).images) {
-      if (i.id == widget.item.imageId[widget.itemNumber-1]) {
-        log(widget.item.imageId[widget.itemNumber-1].toString());
-        setState(() {
-          thisImage = i.imageId; 
-        }
-        );
-      }
-    }
-    // images.add(thisImage);
-    return thisImage;
-  }
-
   late List<Image> images = [];
 
   @override
   Widget build(BuildContext context) {
-                      // String itemImage = 'assets/img/items2/${widget.item.brand}_${widget.item.name}_Item_${widget.itemNumber}.jpg';
-                      // return FittedBox(
-                        
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context) => (ViewImage(images, 0))));
-                          },
-                          child: setItemImage(),
-                        );
-                        // return Image.asset(setItemImage(), fit: BoxFit.contain);
-                        // child: Image.asset(setItemImage(),),
-                        // fit: BoxFit.fill,
-                      // );
+    // String itemImage = 'assets/img/items2/${widget.item.brand}_${widget.item.name}_Item_${widget.itemNumber}.jpg';
+    // return FittedBox(
+//ynt moved folowing function from outer build to inner build
+    Image setItemImage() {
+      // itemType = toBeginningOfSentenceCase(widget.item.type.replaceAll(RegExp(' +'), '_'));
+      // itemName = widget.item.name.replaceAll(RegExp(' +'), '_');
+      // brandName = widget.item.brand.replaceAll(RegExp(' +'), '_');
+      // imageName = 'assets/img/items2/${brandName}_${itemName}_${itemType}_${widget.itemNumber}.jpg';
+      // return imageName;
+      for (ItemImage i
+          in Provider.of<ItemStore>(context, listen: false).images) {
+        for (String j in widget.item.imageId) {
+          if (i.id == j) {
+            images.add(i.imageId);
+          }
+        }
+      }
+      for (ItemImage i
+          in Provider.of<ItemStore>(context, listen: false).images) {
+        if (i.id == widget.item.imageId[widget.itemNumber - 1]) {
+          log(widget.item.imageId[widget.itemNumber - 1].toString());
+          setState(() {
+            thisImage = i.imageId;
+          });
+        }
+      }
+      // images.add(thisImage);
+      return thisImage;
+    }
 
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => (ViewImage(images, 0))));
+      },
+      child: setItemImage(),
+    );
+    // return Image.asset(setItemImage(), fit: BoxFit.contain);
+    // child: Image.asset(setItemImage(),),
+    // fit: BoxFit.fill,
+    // );
   }
 }
