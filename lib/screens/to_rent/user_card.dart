3bd3dc8afebc7c5cbcf34eb4b694561414c668ce
chris <chris@bluemoon.sg>
2025-05-27 21:17:51 +0100

    added circular avatar to rent this page

--- lib/screens/to_rent/user_card.dart
+++ lib/screens/to_rent/user_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:revivals/services/class_store.dart';
 import 'package:revivals/shared/styled_text.dart';
+import 'package:provider/provider.dart';
 
 class UserCard extends StatelessWidget {
   const UserCard(this.ownerName, this.location, {super.key});
@@ -11,15 +13,26 @@ class UserCard extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     String initialLetter = ownerName.substring(0, 1);
+
+    // Get renter profilePicUrl from provider
+    final renter = Provider.of<ItemStoreProvider>(context, listen: false).renter;
+    final profilePicUrl = renter.profilePicUrl;
+
     return Row(
       children: [
         CircleAvatar(
-            backgroundColor: Colors.greenAccent[400],
-            radius: width * 0.04,
-            child: Text(
-              initialLetter,
-              style: const TextStyle(fontSize: 40, color: Colors.white),
-            )), //Text
+          backgroundColor: Colors.greenAccent[400],
+          radius: width * 0.06,
+          backgroundImage: (profilePicUrl != null && profilePicUrl.isNotEmpty)
+              ? NetworkImage(profilePicUrl)
+              : null,
+          child: (profilePicUrl == null || profilePicUrl.isEmpty)
+              ? Text(
+                  initialLetter,
+                  style: TextStyle(fontSize: width * 0.06, color: Colors.white),
+                )
+              : null,
+        ),
         SizedBox(width: width * 0.02),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
